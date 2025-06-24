950d437fd9e9d6e59c10199c4ad11ba5b691f4c7
Liso Wycliff Seth Hlatshwayo <lisowycliff@gmail.com>
2025-06-24 11:59:30 +0200

    first commit

--- modern-solutions/src/router/index.js
+++ modern-solutions/src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import loginView from '@/views/LoginView.vue'
-import LoginView from '@/views/LoginView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
-  }, {
-    path: '/login',
-    name: 'login',
-    component: LoginView
   },
   {
     path: '/about',
